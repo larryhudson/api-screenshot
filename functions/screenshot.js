04dae7e755a68f928829de42ac025f6240a10429
Larry Hudson <larryhudson@hey.com>
2022-10-06 17:22:36 +1100

    test

--- functions/screenshot.js
+++ functions/screenshot.js
@@ -79,7 +79,7 @@ async function screenshot(url, { format, viewport, dpr = 1, withJs = true, wait,
 async function handler(event, context) {
   // e.g. /https%3A%2F%2Fwww.11ty.dev%2F/small/1:1/smaller/
   let pathSplit = event.path.split("/").filter(entry => !!entry);
-  let [name] = pathSplit;
+  let [name, size, aspectratio, zoom, cachebuster] = pathSplit;
   let format = "png"; // hardcoded for now, but png and webp are supported!
   let viewport = [];
 
@@ -87,18 +87,18 @@ async function handler(event, context) {
 
   // Manage your own frequency by using a _ prefix and then a hash buster string after your URL
   // e.g. /https%3A%2F%2Fwww.11ty.dev%2F/_20210802/ and set this to today’s date when you deploy
-  // if(size && size.startsWith("_")) {
-  //   cachebuster = size;
-  //   size = undefined;
-  // }
-  // if(aspectratio && aspectratio.startsWith("_")) {
-  //   cachebuster = aspectratio;
-  //   aspectratio = undefined;
-  // }
-  // if(zoom && zoom.startsWith("_")) {
-  //   cachebuster = zoom;
-  //   zoom = undefined;
-  // }
+  if(size && size.startsWith("_")) {
+    cachebuster = size;
+    size = undefined;
+  }
+  if(aspectratio && aspectratio.startsWith("_")) {
+    cachebuster = aspectratio;
+    aspectratio = undefined;
+  }
+  if(zoom && zoom.startsWith("_")) {
+    cachebuster = zoom;
+    zoom = undefined;
+  }
 
   // Options
   let pathOptions = {};
